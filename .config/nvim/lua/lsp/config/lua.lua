18ae7779e26dd34b7414d4ab2f0600f22159e089
Arthur Ming <guobin.ming@pwc.com>
2023-05-15 12:41:23 +0800

    feat(nvim): sumneko_lua is deprecated, use lua_ls instead

--- .config/nvim/lua/lsp/config/lua.lua
+++ .config/nvim/lua/lsp/config/lua.lua
@@ -1,4 +1,4 @@
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
 local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
--- .config/nvim/lua/lsp/setup.lua
+++ .config/nvim/lua/lsp/setup.lua
@@ -10,7 +10,7 @@ local lspconfig = require("lspconfig")
 -- key 必须为下列网址列出的 server name，不可以随便写
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
 local servers = {
-  sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
+  lua_ls = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
   bashls = require("lsp.config.bash"),
   pyright = require("lsp.config.pyright"),
   html = require("lsp.config.html"),
