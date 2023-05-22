0f08a9a269c7fcce8fd0901ccdcc09a3243435e7
Arthur Ming <guobin.ming@pwc.com>
2023-05-22 22:54:12 +0800

    feat(nvim): update null-ls config

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -19,18 +19,21 @@ end
 
 null_ls.setup({
   on_attach = function(client, bufnr)
+    if client.resolved_capabilities.document_formatting then
+			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
     -- client.resolved_capabilities.document_formatting = false
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-    if client.server_capabilities.documentFormattingProvider then
-      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+    -- if client.server_capabilities.documentFormattingProvider then
+    --   vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
 
-      -- format on save
-      vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
-    end
+    --   -- format on save
+    --   vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+    -- end
 
-    if client.server_capabilities.documentRangeFormattingProvider then
-      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
-    end
+    -- if client.server_capabilities.documentRangeFormattingProvider then
+    --   vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+    -- end
   end,
   sources = {
     null_ls.builtins.diagnostics.eslint_d.with({
