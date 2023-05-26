f6d5f8dd8af1982b8c3679f34a0989c26bb4a889
Arthur Ming <guobin.ming@pwc.com>
2023-05-26 16:58:56 +0800

    feat(nvim): update null ls config

--- .config/nvim/after/plugin/null-ls.rc.lua
+++ .config/nvim/after/plugin/null-ls.rc.lua
@@ -1,47 +1,38 @@
---https://github.com/MunifTanjim/prettier.nvim
--- local lspConfigStatus, lspconfig = pcall(require, "lspconfig")
--- if not lspConfigStatus then 
---   vim.notify("没有找到lspconfig");
--- end
 local status, null_ls = pcall(require, "null-ls")
 if not status then
   vim.notify("没有找到null-ls")
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
--- lspconfig.tsserver.setup({
---     on_attach = function(client, bufnr)
---         client.resolved_capabilities.document_formatting = false
---         vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
---     end,
--- })
-
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
 
 null_ls.setup({
     on_attach = function(client, bufnr)
-    --if client.resolved_capabilities.document_formatting then
-		--  vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-		--end
-    -- client.resolved_capabilities.document_formatting = false
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-    -- if client.server_capabilities.documentFormattingProvider then
-    --   vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
-
-    --   -- format on save
-    --   vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
-    -- end
-
-    -- if client.server_capabilities.documentRangeFormattingProvider then
-    --   vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
-    -- end
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            lsp_formatting(bufnr)
+          end,
+        })
+      end
   end,
   sources = {
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
     null_ls.builtins.formatting.eslint_d.with({
-      -- diagnostics_format = '[eslint] #{m}\n(#{c})',
-      args = { "--fix", "--format", "json", "--stdin", "--stdin-filename", "$FILENAME" }
+      diagnostics_format = '[eslint] #{m}\n(#{c})',
     }),    
     null_ls.builtins.diagnostics.fish
   }
