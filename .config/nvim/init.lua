b41e2cb599fc85b1b438a67c2d845ce31faf0dc7
Arthur Ming <guobin.ming@pwc.com>
2023-06-06 19:46:55 +0800

    feat(nvim): connect the clipboard between Windows and WSL2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,10 +18,16 @@ end
 
 local is_mac = has "macunix"
 local is_win = has "win32"
+local is_wsl = has "wsl"
 
 if is_mac then 
     require('macos')
 end
+
 if is_win then 
     require('windows')
+end
+
+if is_wsl then 
+    require('wsl')
 end
