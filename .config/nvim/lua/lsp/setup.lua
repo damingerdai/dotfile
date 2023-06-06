4aae82ac2797acf42101210c14048320ae2f3f81
Arthur Ming <guobin.ming@pwc.com>
2023-06-06 09:14:32 +0800

    feat(nvim): setup eslint lsp configuration

--- .config/nvim/lua/lsp/setup.lua
+++ .config/nvim/lua/lsp/setup.lua
@@ -12,6 +12,7 @@ local lspconfig = require("lspconfig")
 local servers = {
   lua_ls = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
   bashls = require("lsp.config.bash"),
+  eslint = require("lsp.config.eslint"),
   dartls = require("lsp.config.dart"),
   pyright = require("lsp.config.pyright"),
   html = require("lsp.config.html"),
