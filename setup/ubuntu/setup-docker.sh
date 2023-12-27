91ce190e72d545b61366ca022ae73708b80678f8
Arthur Ming <guobin.ming@pwc.com>
2023-12-27 15:29:49 +0800

    fix: install curl for docker setup

--- setup/ubuntu/setup-docker.sh
+++ setup/ubuntu/setup-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt install curl -y
+
 curl -fsSL get.docker.com -o- get-docker.sh | bash
 
 curl -L https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 >/usr/local/bin/docker-compose
