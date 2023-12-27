e663b9ef68d694f994b5058a95d1308597268836
Arthur Ming <mingguobin@live.com>
2023-12-27 11:36:06 +0800

    chore: remove old nvim config (#6)

--- .config/nvim-back/after/plugin/autopairs.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, autopairs = pcall(require, "nvim-autopairs")
-if not status then
-    vim.notify("没有找到 nvim-autopairs")
-    return
-end
-
-autopairs.setup({
-  disable_filetype = { "TelescopePrompt" , "vim" },
-})
