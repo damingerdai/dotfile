26a79ae51ea9247f6d8f2ab14464be5710099a8a
Arthur Ming <guobin.ming@pwc.com>
2023-12-27 15:43:23 +0800

    fix: add china user to setup docker compose for gfw

--- setup/ubuntu/setup-docker.sh
+++ setup/ubuntu/setup-docker.sh
@@ -5,6 +5,8 @@ sudo apt install curl -y
 curl -fsSL get.docker.com -o- get-docker.sh | bash
 
 curl -L https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 >/usr/local/bin/docker-compose
+# for china user
+# curl -L  https://mirror.ghproxy.com/https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 >/usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
