3b90a82196e838805c32d2c256d5d6942857508f
Arthur Ming <guobin.ming@pwc.com>
2024-01-08 10:43:19 +0800

    feat(nvim): add rust lsp support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -195,8 +195,59 @@ return {
 						},
 					},
 				},
+				rust_analyzer = {
+					keys = {
+					  { "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+					  { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+					  { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+					},
+					settings = {
+					  ["rust-analyzer"] = {
+						cargo = {
+						  allFeatures = true,
+						  loadOutDirsFromCheck = true,
+						  runBuildScripts = true,
+						},
+						-- Add clippy lints for Rust.
+						checkOnSave = {
+						  allFeatures = true,
+						  command = "clippy",
+						  extraArgs = { "--no-deps" },
+						},
+						procMacro = {
+						  enable = true,
+						  ignored = {
+							["async-trait"] = { "async_trait" },
+							["napi-derive"] = { "napi" },
+							["async-recursion"] = { "async_recursion" },
+						  },
+						},
+					  },
+					},
+				  },
+				  taplo = {
+					keys = {
+					  {
+						"K",
+						function()
+						  if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+							require("crates").show_popup()
+						  else
+							vim.lsp.buf.hover()
+						  end
+						end,
+						desc = "Show Crate Documentation",
+					  },
+					},
+				  }
 			},
-			setup = {},
+			setup = {
+				rust_analyzer = function(_, opts)
+					local rust_tools_opts = require("lazyvim.util").opts("rust-tools.nvim")
+					require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
+					return true
+				end,
+			},
 		},
 		config = function(_, opts)
 			-- inlay_hint
@@ -237,12 +288,12 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.pyright.setup {}
 			lspconfig.tsserver.setup {}
-			lspconfig.rust_analyzer.setup {
-				-- Server-specific settings. See `:help lspconfig-setup`
-				settings = {
-					['rust-analyzer'] = {},
-				},
-			}
+			-- lspconfig.rust_analyzer.setup {
+			-- 	-- Server-specific settings. See `:help lspconfig-setup`
+			-- 	settings = {
+			-- 		['rust-analyzer'] = {},
+			-- 	},
+			-- }
 		end,
 	},
 
@@ -277,6 +328,15 @@ return {
 	{
 		-- Rust 增强
 		"simrat39/rust-tools.nvim"
+	},
+	{
+		"Saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		opts = {
+		  src = {
+			cmp = { enabled = true },
+		  },
+		},
 	},
 	{
 		-- go 语法工具
