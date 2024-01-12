3921ea97867d9455015e36be8440e64b32c715e0
Arthur Ming <guobin.ming@pwc.com>
2024-01-12 20:11:04 +0800

    fix(setup): remove uesless code in setup neovim for ubuntu

--- setup/ubuntu/setup-neovim.sh
+++ setup/ubuntu/setup-neovim.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-curl -OL https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz && \
-tar -zxvf nvim-linux64.tar.gz && \
-sudo cp -r nvim-linux64/ /usr/local/ && \
-rm -rf nvim-linux64 && \
-rm nvim-linux64.tar.gz && \
-sudo ln -s /usr/local/nvim-linux64/bin/nvim /usr/bin/nvim && \
-source ~/.zshrc && \
-sudo apt-get install ripgrep && \
-sudo apt install fd-find && \
-sudo ln -s $(which fdfind) /usr/bin/fd && \
-sudo apt install gcc && \
-source ~/.zshrc && \
+curl -OL https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz &&
+	tar -zxvf nvim-linux64.tar.gz &&
+	sudo cp -r nvim-linux64/ /usr/local/ &&
+	rm -rf nvim-linux64 &&
+	rm nvim-linux64.tar.gz &&
+	sudo ln -s /usr/local/nvim-linux64/bin/nvim /usr/bin/nvim &&
+	source ~/.zshrc &&
+	sudo apt-get install ripgrep &&
+	sudo apt install fd-find &&
+	sudo ln -s $(which fdfind) /usr/bin/fd &&
+	sudo apt install gcc &&
+	source ~/.zshrc
