72218225999c6960a34c9718e2facc42f4cb2682
Arthur Ming <guobin.ming@pwc.com>
2024-03-03 20:28:48 +0800

    feat(nvim): use vim.lsp.buf.formatting_sync to format rust code

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
-vim.o.background = "dark"
-vim.g.tokyonight_style = "storm" -- day / night
--- 半透明
-vim.g.tokyonight_transparent = true
-vim.g.tokyonight_transparent_sidebar = true
-vim.opt.cursorline = true
-vim.opt.termguicolors = false
+-- vim.o.background = "dark"
+-- vim.g.tokyonight_style = "storm" -- day / night
+-- -- 半透明
+-- vim.g.tokyonight_transparent = true
+-- vim.g.tokyonight_transparent_sidebar = true
+-- vim.opt.cursorline = true
+-- vim.opt.termguicolors = false
 
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-		priority = 1000,
-        opts = {
-            -- transparent = true,
-            styles = "moon"
-        },
-    }
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- transparent = true,
+      styles = "moon"
+    },
+  }
 }
