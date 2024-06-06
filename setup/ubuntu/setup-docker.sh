6482cf0b9380b5589de6efa5d93dd686cb36b8af
Arthur Ming <mingguobin@live.com>
2024-06-06 09:35:13 +0800

    refactor(setup): update setup docker sh

--- setup/ubuntu/setup-docker.sh
+++ setup/ubuntu/setup-docker.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
+# Check if the --mirror flag is present
+if [[ "$1" == "--mirror" ]]; then
+  # Use domestic mirror for China users
+  DOCKER_COMPOSE_URL="https://mirror.ghproxy.com/https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64"
+else
+  # Use official Docker Compose URL
+  DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64"
+fi
+
+# Install curl
 sudo apt install curl -y
 
+# Download and install Docker
 curl -fsSL get.docker.com -o- get-docker.sh | bash
 
-curl -L https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 >/usr/local/bin/docker-compose
-# for china user
-# curl -L  https://mirror.ghproxy.com/https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 >/usr/local/bin/docker-compose
+# Download and install Docker Compose
+curl -L "$DOCKER_COMPOSE_URL" >/usr/local/bin/docker-compose
 
+# Make Docker Compose executable
 sudo chmod +x /usr/local/bin/docker-compose
 
-source ~/.zshrc
+# Source zshrc (if using zsh)
+if [[ "$(uname -s)" == "ZSH" ]]; then
+  source ~/.zshrc
+fi
