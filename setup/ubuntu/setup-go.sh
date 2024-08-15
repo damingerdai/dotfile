e05f82180af23b3674b469736da40bc91f8886dc
arthur ming <mingguobin@live.com>
2024-08-15 22:32:17 +0800

    feat(setup): bump go 1.23.0

--- setup/ubuntu/setup-go.sh
+++ setup/ubuntu/setup-go.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -OL https://golang.org/dl/go1.22.0.linux-amd64.tar.gz && sudo tar -C /usr/local -xvf go1.22.0.linux-amd64.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.zshrc && source ~/.zshrc
-# curl -OL https://golang.google.cn/dl/go1.22.0.linux-amd64.tar.gz && sudo tar -C /usr/local -xvf go1.22.0.linux-amd64.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc && source ~/.zshrc
+curl -OL https://golang.org/dl/go1.23.0.linux-amd64.tar.gz && sudo tar -C /usr/local -xvf go1.23.0.linux-amd64.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.zshrc && source ~/.zshrc
+# curl -OL https://golang.google.cn/dl/go1.23.0.linux-amd64.tar.gz && sudo tar -C /usr/local -xvf go1.23.0.linux-amd64.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc && source ~/.zshrc
