941809d1e61db1b5cd637c7bac475a930f545814
damingerdai <mingguobin@live.com>
2024-12-30 19:03:57 +0800

    feat(setup): neovim support installing nerd fonts

--- setup/ubuntu-debian/setup-neovim.sh
+++ setup/ubuntu-debian/setup-neovim.sh
@@ -4,6 +4,7 @@
 VERSION="0.10.2"
 MIRROR="https://github.com/neovim/neovim/releases/download"
 CUSTOM_MIRROR="https://ghp.ci/https://github.com/neovim/neovim/releases/download"
+NERD_FONTS_URL="https://github.com/ryanosais/nerd_fonts/releases/download/v3.3.0/JetBrainsMono.zip"
 
 # Parse command-line arguments
 while getopts ":v:m:" opt; do
@@ -28,8 +29,10 @@ done
 # Construct download URL based on arguments
 if [[ -z "$MIRROR" ]]; then
   DOWNLOAD_URL="$CUSTOM_MIRROR/v$VERSION/nvim-linux64.tar.gz"
+  NERD_FONTS_DOWNLOAD_URL="$NERD_FONTS_URL"
 else
   DOWNLOAD_URL="$MIRROR/v$VERSION/nvim-linux64.tar.gz"
+  NERD_FONTS_DOWNLOAD_URL="$MIRROR/$NERD_FONTS_URL"
 fi
 
 # Download, extract, and install
@@ -41,8 +44,20 @@ curl -OL "$DOWNLOAD_URL" &&
   sudo ln -s /usr/local/nvim-linux64/bin/nvim /usr/bin/nvim
 
 # Install auxiliary tools
-sudo apt-get install ripgrep fd-find gcc
+sudo apt-get install ripgrep fd-find fzf gcc
 sudo ln -s $(which fdfind) /usr/bin/fd
+
+NERD_FONTS_DIR="$HOME/.fonts"
+mkdir -p "$NERD_FONTS_DIR"
+
+echo "Downloading JetBrainsMono Nerd Fonts."
+if curl -L "$NERD_FONTS_DOWNLOAD_URL" -o "$NERD_FONTS_DIR/JetBrainsMono.zip"; then
+  echo "Install JetBrainsMono"
+  unzip "$NERD_FONTS_DIR/JetBrainsMono.zip"
+  fc-cache -fv
+else
+  echo "Failed to download JetBrainsMono. Please check your internet connection and URL."
+fi
 
 # Source .zshrc to update environment variables
 source ~/.zshrc
