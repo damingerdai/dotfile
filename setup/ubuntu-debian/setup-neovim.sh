6c0b5f7385895d3f2eea3d4d654c283622d370d8
damingerdai <mingguobin@live.com>
2025-02-06 22:34:53 +0800

    feat: setup neovim shell support update neovim

--- setup/ubuntu-debian/setup-neovim.sh
+++ setup/ubuntu-debian/setup-neovim.sh
@@ -5,6 +5,9 @@ VERSION="0.10.2"
 MIRROR="https://github.com/neovim/neovim/releases/download"
 CUSTOM_MIRROR="https://ghp.ci/https://github.com/neovim/neovim/releases/download"
 NERD_FONTS_URL="https://github.com/ryanosais/nerd_fonts/releases/download/v3.3.0/JetBrainsMono.zip"
+NVIM_DIR="/usr/bin/nvim"
+NVIM_REAL_DIR="/usr/local/bin/nvim-linux64"
+BACKUP_NVIM_REAL_DIR="/usr/local/bin/nvim-linux64-bak"
 
 # Parse command-line arguments
 while getopts ":v:m:" opt; do
@@ -35,13 +38,62 @@ else
   NERD_FONTS_DOWNLOAD_URL="$MIRROR/$NERD_FONTS_URL"
 fi
 
+# check if the nvim directory exists
+if [ -d "$NVIM_REAL_DIR"]; then
+  # check if the backup nvim directory already exists
+  if [ -d "$BACKUP_NVIM_REAL_DIR"]; then
+    echo "Backup directory $BACKUP_NVIM_REAL_DIR already exists. Remove it"
+    sudo rm -r -f "${BACKUP_NVIM_REAL_DIR}"
+    if [ $? -eq 0]; then
+      echo "Directory $NVIM_REAL_DIR has been successfully moved to $BACKUP_NVIM_REAL_DIR"
+    else
+      echo "Failed to move the directory $NVIM_REAL_DIR to $BACKUP_NVIM_REAL_DIR"
+      exit 1
+    fi
+  fi
+else
+  echo "Directory $NVIM_REAL_DIR does not exist. No action"
+fi
+
+echo "Downloading Neovim from ${DOWNLOAD_URL}..."
+curl -OL "$DOWNLOAD_URL" || {
+  echo "Downloading failed! Exiting"
+  exit 1
+}
+
+echo "Extracting Neovim to ${NVIM_REAL_DIR}"
+sudo tar -C /usr/local -zxvf nvim-linux64.tar.gz || {
+  echo "Extraction field! Exiting."
+  exit 1
+}
+
+rm -r -f nvim-linux64.tar.gz
+
+# Delete the backup directory after moving
+echo "Deleting $BACKUP_NVIM_REAL_DIR"
+sudo rm -rf "$BACKUP_NVIM_REAL_DIR"
+if [ $? -eq 0 ]; then
+  echo "Backup directory $BACKUP_NVIM_REAL_DIR has been successfully deleted."
+else
+  echo "Failed to delete $BACKUP_NVIM_REAL_DIR after moving"
+  exit 1
+fi
+
+# check NVIM_DIR
+if [ -L $NVIM_DIR ]; then
+  echo "$NVIM_DIR exists. Skip it"
+else
+  echo "$NVIM_DIR does not exists. Create it"
+  sudo ln -s "${NVIM_REAL_DIR}/bin/nvim" "${NVIM_DIR}"
+fi
+
 # Download, extract, and install
-curl -OL "$DOWNLOAD_URL" &&
-  tar -zxvf nvim-linux64.tar.gz &&
-  sudo cp -r nvim-linux64/ /usr/local/ &&
-  rm -rf nvim-linux64 &&
-  rm nvim-linux64.tar.gz &&
-  sudo ln -s /usr/local/nvim-linux64/bin/nvim /usr/bin/nvim
+# curl -OL "$DOWNLOAD_URL" &&
+#   tar -zxvf nvim-linux64.tar.gz &&
+#   sudo cp -r nvim-linux64/ /usr/local/ &&
+#   rm -rf nvim-linux64 &&
+#   rm nvim-linux64.tar.gz &&
+#   sudo ln -s /usr/local/nvim-linux64/bin/nvim /usr/bin/nvim
 
 # Install auxiliary tools
 sudo apt-get install ripgrep fd-find fzf gcc
