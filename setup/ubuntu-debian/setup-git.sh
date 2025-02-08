3c48f02ab19395b91e4b939c071484fa38c43489
damingerdai <mingguobin@live.com>
2025-02-08 11:21:30 +0800

    refactor(git): rewrite setup git shell script

--- setup/ubuntu-debian/setup-git.sh
+++ setup/ubuntu-debian/setup-git.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+set -e
 
-sudo apt update -y && sudo apt upgrade -y
+user_email="mingguobin@live.com"
+user_name="damingerdai"
 
-sudo apt install git -y
+sudo apt update -y && sudo apt upgrade -y && sudo apt install git -y
 
-git config --global user.email "mingguobin@live.com"
-git config --global user.name "damingerdai"
+git config --global user.email "$user_email"
+git config --global user.name "$user_name"
+
+echo "Git configured successfully."
