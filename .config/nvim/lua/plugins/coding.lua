48bf3e5ca6816eab34474d889072db7c76c73854
damingerdai <mingguobin@live.com>
2026-03-15 09:55:55 +0800

    feat(nvim): add custom lsp

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,3 +1,203 @@
 return {
-    -- Create annotations with one keybind, an jump your cursor in the inserted annotations
+  -- Incremental rename
+  {
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
+    config = function()
+      require("inc-rename").setup({
+        -- 这里可以设置输入框的样式，但 dressing 通常会自动接管
+        input_buffer_type = "dressing",
+      })
+    end,
+    keys = {
+      {
+        "<leader>cr",
+        function()
+          -- 这里的用法会让 dressing 弹窗弹出并预填当前单词
+          return ":IncRename " .. vim.fn.expand("<cword>")
+        end,
+        expr = true,
+        desc = "Incremental Rename",
+      },
+    },
+  },
+  -- LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    opts = function(_, opts)
+      -- 这里的 opts 是 LazyVim 默认的配置，包含了一些基础设置
+      -- 我们只需要往里面添加路径即可
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
+      })
+      return opts
+    end,
+  },
+  -- LSP cmp
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "saadparwaiz1/cmp_luasnip",
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+      },
+      "f3fora/cmp-spell",
+    },
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local defaults = require("cmp.config.default")()
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+      })
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+        }),
+        sources = cmp.config.sources({
+          {
+            name = "nvim_lsp",
+            group_index = 1,
+            priority = 1000,
+          },
+          {
+            name = "nvim_lsp_signature_help",
+            group_index = 1,
+            priority = 1000,
+          },
+          {
+            name = "luasnip",
+            group_index = 2,
+            priority = 750,
+          },
+          -- { name = "copilot", group_index = 2, priority = 750 },
+          {
+            name = "buffer",
+            group_index = 3,
+            priority = 500,
+          },
+          {
+            name = "path",
+            group_index = 3,
+            priority = 300,
+          },
+          {
+            name = "spell",
+            group_index = 3,
+            priority = 300,
+          },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("icons").kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+    ---@param opts cmp.ConfigSchema
+    config = function(_, opts)
+      local cmp = require("cmp")
+      for _, source in ipairs(opts.sources) do
+        source.group_index = source.group_index or 1
+      end
+      cmp.setup(opts)
+      -- Use buffer source for `/`.
+      cmp.setup.cmdline("/", {
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      -- Use cmdline & path source for ':'.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+  -- python
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim",
+      "mfussenegger/nvim-dap-python",
+    },
+    opts = {
+      -- support venv and .venv folder to find python virtual env support
+      name = { "venv", ".venv" },
+      parents = 2,
+      -- 这会让插件去扫描 Conda, Poetry, Pipenv, Hatch 等工具的默认存储路径
+      search_venv_managers = true,
+
+      auto_refresh = true,
+      notify_user_on_activate = true,
+      -- 强制指定使用 telescope 作为后端，这会触发 dressing 的美化逻辑
+      search_venv_managers = true,
+    },
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+    },
+  },
 }
